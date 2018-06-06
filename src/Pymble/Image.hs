f3ed60b0bee4dea50549ad3fc70d1541aea86034
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-06-06 16:16:22 -0700

    convert dynamic image to classical 8bit RBGA

--- src/Pymble/Image.hs
+++ src/Pymble/Image.hs
@@ -2,10 +2,12 @@
 
 module Pymble.Image (
     download
+  , normalize
 
 ) where
 
-import Codec.Picture as P
+import Codec.Picture       as P
+import Codec.Picture.Types as PT
 import Network.HTTP.Simple as NW
 
 ----------------------------------------------------------------------
@@ -22,4 +24,19 @@ download uri = do
       contentType = NW.getResponseHeader "Content-Type" response
       body        = NW.getResponseBody response
   
-  either error (return . id) (decodeImage body)
+  either error return $ decodeImage body
+
+
+-- | Converts generic image to image with the classical
+-- pixels (8bit red, green, blue and alpha).
+--
+normalize :: P.DynamicImage -> Maybe (P.Image P.PixelRGBA8)
+normalize dynamicImage =
+  case dynamicImage of
+    P.ImageY8     i -> Just $ PT.promoteImage i
+    P.ImageYA8    i -> Just $ PT.promoteImage i
+    P.ImageRGB8   i -> Just $ PT.promoteImage i
+    P.ImageRGBA8  i -> Just i
+    P.ImageYCbCr8 i -> Just $ PT.promoteImage (PT.convertImage i :: P.Image P.PixelRGB8)
+    P.ImageCMYK8  i -> Just $ PT.promoteImage (PT.convertImage i :: P.Image P.PixelRGB8)
+    _               -> Nothing

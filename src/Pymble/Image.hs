7bc0d948acb78bd5edb8617b2dd3a9c49311ec73
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-06-06 17:06:47 -0700

    converting image to and from repa array

--- src/Pymble/Image.hs
+++ src/Pymble/Image.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Pymble.Image (
     download
@@ -8,9 +9,14 @@ module Pymble.Image (
 
 import Codec.Picture       as P
 import Codec.Picture.Types as PT
+import Data.Array.Repa     as R
+import Data.Array.Repa ((:.), (!))
 import Network.HTTP.Simple as NW
 
 ----------------------------------------------------------------------
+
+type RGBA8 = (P.Pixel8, P.Pixel8, P.Pixel8, P.Pixel8)
+
 
 -- | Downloads the image with the specified URI
 -- and converts it to 'P.DynamicImage'.
@@ -40,3 +46,28 @@ normalize dynamicImage =
     P.ImageYCbCr8 i -> Just $ PT.promoteImage (PT.convertImage i :: P.Image P.PixelRGB8)
     P.ImageCMYK8  i -> Just $ PT.promoteImage (PT.convertImage i :: P.Image P.PixelRGB8)
     _               -> Nothing
+
+
+-- | Converts 'P.Image' to repa 'R.Array' for further processing.
+--
+toArray :: P.Image P.PixelRGBA8 -> R.Array R.D R.DIM2 RGBA8
+toArray img@Image {..} =
+  R.fromFunction (R.Z :. imageWidth :. imageHeight)
+    (\(R.Z :. x :. y) ->
+      let (P.PixelRGBA8 r g b a) = P.pixelAt img x y
+      in (r, g, b, a))
+
+
+-- | Converts repa 'R.Array' to resulting 'P.Image'.
+--
+fromArray :: R.Array R.U R.DIM2 RGBA8 -> P.Image P.PixelRGBA8
+fromArray array =
+    P.generateImage mkPixel width height
+  where
+    -- image size
+    R.Z :. width :. height = R.extent array
+    -- given the array coordinates creates corresponding image pixel
+    mkPixel x y =
+      let (r, g, b, a) = array ! (Z :. x :. y)
+      in P.PixelRGBA8 r g b a
+      

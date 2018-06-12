ca00d0fc0f298b5ab60c1957a06c95fc693cba2c
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-06-12 12:08:12 -0700

    find the character that is the best fit for the given brightness

--- src/Pymble/Image/Convert.hs
+++ src/Pymble/Image/Convert.hs
@@ -37,6 +37,9 @@ import           Data.Array.Repa     as R   hiding (map)
 import qualified Data.Map.Lazy       as ML
 import           Data.Word
 import           Data.List (foldl')
+import           Data.Foldable (minimumBy)
+import           Data.Function (on)
+import           Data.Tuple (swap)
 ----------------------------------------------------------------------
 
 
@@ -184,7 +187,15 @@ averageBrightness =
 -- | Having the 'BrightnessMap', finds the
 -- character that is the best fit for the given 'Brightness'.
 bestFit :: BrightnessMap -> Brightness -> Char
-bestFit = undefined
+bestFit bmap brightness =
+    snd $ ML.foldrWithKey pickBest (0, ' ') bmap
+  where
+    -- compare the current best fit with the
+    -- new candidate and pick the best of them
+    pickBest b c best = minimumBy (compare `on` (score . fst)) ((b, c), best)
+
+    -- scores a bestfit brightness candidate
+    score candidate = abs $ (fromIntegral brightness) - (fromIntegral candidate)
 
 
 -- | From ASCII art point of view this is basically the same as 'toDelayedAsciiArt',

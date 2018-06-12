b8e679a7a44e9a9a1111578cc0d30362af9da317
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-06-12 11:18:15 -0700

    generate fontspec for the given dictionary

--- fontspec/fontspec.py
+++ fontspec/fontspec.py
@@ -1,12 +1,12 @@
 """Font specification generator.
 
 This module exposes functionality to generate a font specification
-that is used by the pymble server to convert an images into colored ASCII art.
+that is used by the pymble server to convert images into colored ASCII art.
 
-Each font has a different representation of a characters and as a result
-the same character in different fonts is represents using different set of
-pixels. This means that the same character in different fonts could have
-a different 'brightness' value.
+Each font has a different representation of the characters and as a result
+the same character in the different fonts could be represented using different set of
+pixels. As a consequence it means that the same character rendered using
+different fonts could have a different 'brightness' value.
 
 For more presice conversion of an image into ASCII art it is important to
 know the font that is used to output the resulting ASCII art.
@@ -27,13 +27,15 @@ from PIL import Image, ImageDraw, ImageFont
 def create_char_image(char, font_path,
                       image_size=(90, 160), font_size=120,
                       background=(0, 0, 0), foreground=(255, 255, 255)):
-    """Renders the specified char to an image using the provided font.
+    """Renders the specified character of the provided font to an image.
 
     Args:
-        char (str): The character to render.
+        char (str): The character to render as an image.
         font_path (str): The path to the ttf or otf font file.
-        image_size ((int, int)): Size of the generated image. Defaults to (90, 160).
-        font_size (int): Font size. Defaults to 120.
+        image_size ((int, int)): Dimensions of the generated image. Defaults to (90, 160).
+        font_size (int): The font size that is used to render the image.
+            In practice big letters rendered on hi dimension image gives more
+            accurate results. Defaults to 120.
         background ((int, int, int)): Background color of the generated image (R, G, B).
             Defaults to black color (0, 0, 0).
         foreground ((int, int, int)): The color of the rendered character (R, G, B).
@@ -54,14 +56,14 @@ def create_char_image(char, font_path,
 
 
 def char_brightness(char_image):
-    """Given the rendered char image, computes the total
-    average char brightness.
+    """Given the image with the rendered character, computes the total
+    average brightness.
 
     Args:
-        char_image (Image): The image of the rendered char.
+        char_image (Image): The image with the rendered char.
 
     Returns:
-        int: total average char brightness.
+        int: total average brightness of the image (basically of the char).
 
     """
 
@@ -76,7 +78,7 @@ def char_brightness(char_image):
 
 
 def eval_brightness_dict(dictionary, font_path):
-    """Given a collection with characters and a font,
+    """Given a collection of characters and a font,
     computes the brightness of every character in the
     collection.
 
@@ -94,11 +96,11 @@ def eval_brightness_dict(dictionary, font_path):
 
 
 def normalize_brightness_dict(brightness_dict):
-    """Usually the distribution of char brightness for a given font
+    """Usually the distribution of the character brightness for a given font
     is not as diverse as we would like it to be. This results in a pretty
-    pure result during image to ASCII art conversion (if used as-is).
+    poor result during the image to ASCII art conversion (if used as-is).
 
-    Because of this it's much better to normalize the brightness dictionary.
+    Because of this it's much better to normalize the brightness distribution.
     Normalization widens the distribution ranges to 8-bit 0-255 range.
 
     Args:
@@ -115,14 +117,14 @@ def normalize_brightness_dict(brightness_dict):
         b_max = max(b_max, brightness)
 
     def widen(b):
-        """Using min and max bound widens the char brightness"""
+        """Using the min and max bounds to widen the char brightness"""
         return int(round( ( (b - b_min) / (b_max - b_min) ) * 255 ))
 
     return {char: widen(brightness) for char, brightness in brightness_dict.items()}
 
 
 def save_fontspec(brightness_dict, file_path='spec.txt'):
-    """Given the character brightness dictionary outputs it
+    """Given the character brightness dictionary, outputs it
     to simple text file. The content of the generated file could be copy-pasted
     directly into haskell source code.
 
@@ -132,7 +134,8 @@ def save_fontspec(brightness_dict, file_path='spec.txt'):
 
     Args:
         brightness_dict (dict): A map of characters to brightness.
-        file_pat (str): Path of the file to save the generated fontspec.
+        file_path (str): Path to the file to save the generated fontspec.
+            Defaults to 'spec.txt'.
     """
     with open(file_path, 'w+') as f:
         f.write('[ ')
@@ -140,7 +143,26 @@ def save_fontspec(brightness_dict, file_path='spec.txt'):
         first = True
         for char, brightness in sorted(brightness_dict.items(), key=lambda x: x[1]):
             if not first: f.write(', ')
-            f.write('({char}, {brightness})\n'.format(char=char, brightness=brightness))
+            f.write("('{char}', {brightness})\n".format(char=char, brightness=brightness))
             first = False
 
         f.write(']')
+
+
+def generate_fontspec(dictionary, font_path='cour.ttf', file_path='spec.txt'):
+    """Generates and saves to file the specification of the specified font
+    for the given collection of characters.
+
+    Args:
+        dictionary (list): A collection of characters that would be used
+            as a dictionary during the image conversion to ASCII art.
+        font_path (str): Path to the font that is used to render the
+            dictionary with characters. Defaults to 'cour.ttf'.
+        file_path (str): Path to the file that would have the
+            generated fontspec. Defaults to 'spec.txt'.
+        
+    """
+    brightness_dict      = eval_brightness_dict(dictionary, font_path)
+    nomalized_brightness = normalize_brightness_dict(brightness_dict)
+
+    save_fontspec(nomalized_brightness, file_path)

a24e21cead16bfc80e6e254e2e7640328bd57a0a
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-06-21 11:36:27 -0700

    encode terminal color skeleton

--- src/Pymble/PrettyPrint/Telnet.hs
+++ src/Pymble/PrettyPrint/Telnet.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 -- |
 --
 module Pymble.PrettyPrint.Telnet
@@ -7,6 +8,33 @@ module Pymble.PrettyPrint.Telnet
     ) where
 
 import Data.Array.Repa         as R
+
 import Pymble.Image.Convert
+import Pymble.PrettyPrint.Telnet.Color
 ----------------------------------------------------------------------
 
+-- |
+--
+encodeColoredChar :: ColoredChar -> ShowS
+encodeColoredChar (char, color) = undefined
+
+
+-- |
+--
+encodeTerminalColor :: TerminalColor -> ShowS
+encodeTerminalColor = \case
+    Color16 c       -> showString "\ESC[" . shows (adj c) . end
+    Xterm256 c      -> showString "\ESC[38;5;" . shows c . end
+    Grayscale c     -> showString "\ESC[38;5;" . shows c . end
+    TrueColor r g b -> showString "\ESC[38;2;" .
+                          shows r . showString ";" .
+                          shows g . showString ";" .
+                          shows b . end
+  where
+    adj x = if x < 8 then x + 30 else x + 90 
+    end = showString "m"
+
+-- |
+--
+encodeCharacter :: Char -> ShowS
+encodeCharacter = showChar

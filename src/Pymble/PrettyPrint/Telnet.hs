1a96095603efbe002b74a7928a4d70062b00081b
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-06-21 17:23:12 -0700

    pretty print skeleton

--- src/Pymble/PrettyPrint/Telnet.hs
+++ src/Pymble/PrettyPrint/Telnet.hs
@@ -4,24 +4,73 @@
 --
 module Pymble.PrettyPrint.Telnet
     (
+    -- *
+      ColoredTermChar
+    , convert
+    , prettyPrint
     -- * Character and color encoding
-      encodeColoredChar
+    , encodeColoredChar
     , encodeTerminalColor
     , encodeChar
     , encodeReset
-
+    -- * Terminal control sequences
+    , termClear
     ) where
 
-import Data.Array.Repa         as R
+import qualified Data.Array.Repa as R
+import           Data.Tuple (swap)
 
 import Pymble.Image.Convert
-import Pymble.PrettyPrint.Telnet.Color
+import qualified Pymble.PrettyPrint.Telnet.Color as TC
 ----------------------------------------------------------------------
+
+
+-- |
+--
+data ColorScheme =
+    Color16
+  | Xterm256
+  | Grayscale
+  | TrueColor
+
+
+-- |
+--
+type ColoredTermChar = (Char, TC.TerminalColor)
+
+
+-- |
+--
+-- convert :: (ColoredChar -> ColoredTermChar)
+--               -> DelayedArray ColoredChar
+--               -> DelayedArray ColoredTermChar
+-- convert = R.map
+
+
+convert :: DelayedArray ColoredTermChar -> DelayedArray String
+convert = R.map $ \c -> (uncurry $ flip $ encodeColoredChar) c ""
+
+
+-- | Pretty prints the delayed array of 'ColoredTermChar' to
+-- the delayed array of terminal-encoded strings.
+--
+prettyPrint :: ColorScheme -> DelayedArray ColoredChar -> IO String
+prettyPrint colorScheme array = do
+    let coloredArray = R.map (fmap converter) array
+        converted    = convert coloredArray
+    -- R.foldAllP (++) [] converted
+    undefined
+  where
+    converter = case colorScheme of
+      Color16   -> TC.toStandard16
+      Xterm256  -> TC.toXterm256
+      Grayscale -> TC.toGrayscale
+      TrueColor -> TC.toTrueColor
 
 
 -- | Encodes character to be rendered with the specified 'TerminalColor'.
 --
-encodeColoredChar :: TerminalColor -> Char -> ShowS
+encodeColoredChar :: TC.TerminalColor -> Char -> ShowS
 encodeColoredChar color char =
   encodeTerminalColor color . encodeChar char . encodeReset
 
@@ -30,12 +79,12 @@ encodeColoredChar color char =
 -- following the sequence would have the specified
 -- foreground color.
 --
-encodeTerminalColor :: TerminalColor -> ShowS
+encodeTerminalColor :: TC.TerminalColor -> ShowS
 encodeTerminalColor = \case
-    Color16 c       -> ss "\ESC["      . shows (toC c) . ss "m"
-    Xterm256 c      -> ss "\ESC[38;5;" . shows c       . ss "m"
-    Grayscale c     -> ss "\ESC[38;5;" . shows c       . ss "m"
-    TrueColor r g b -> ss "\ESC[38;2;" . shows r . ss ";" .
+    TC.Color16 c       -> ss "\ESC["      . shows (toC c) . ss "m"
+    TC.Xterm256 c      -> ss "\ESC[38;5;" . shows c       . ss "m"
+    TC.Grayscale c     -> ss "\ESC[38;5;" . shows c       . ss "m"
+    TC.TrueColor r g b -> ss "\ESC[38;2;" . shows r . ss ";" .
                                          shows g . ss ";" .
                                          shows b . ss "m"
   where
@@ -57,4 +106,10 @@ encodeChar = showChar
 -- | Reset all attributes off.
 --
 encodeReset :: ShowS
-encodeReset = showString "\ESC[0;00m" 
+encodeReset = showString "\ESC[0;00m"
+
+
+-- | Clear terminal.
+--
+termClear :: ShowS
+termClear = showString "\ESC[2J"

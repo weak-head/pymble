cb5cf62067fcf28181c2f201d17e99f6843c9bb2
Oleksandr Zinchenko <weak.head.nf@gmail.com>
2018-06-28 15:22:51 -0700

    error handling for http download

--- src/Pymble/Image/Storage.hs
+++ src/Pymble/Image/Storage.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Pymble.Image.Storage
     (
@@ -12,12 +13,16 @@ module Pymble.Image.Storage
 import Prelude hiding (read)
 import Control.Monad (join)
 import Codec.Picture
+import qualified Control.Exception as E
+import Network.HTTP.Conduit
 import Network.HTTP.Simple
+import Network.HTTP.Types.Status
+import Network.URI (isURI)
 import System.Directory (doesFileExist)
 ----------------------------------------------------------------------
 
--- | A facade for loading images from both 
--- local and remote sources.
+-- | A facade for loading and decoding images
+-- from both local and remote sources.
 --
 load :: String -> IO DynamicImage
 load url = do
@@ -27,21 +32,44 @@ load url = do
     False -> download url
 
 
--- | Read and load an image file, converting the image
--- to 'P.DynamicImage'.
+-- | Read the image from the file system
+-- and decode it to 'P.DynamicImage'.
 --
 read :: String -> IO DynamicImage
 read path =
   join $ either error return <$> readImage path
 
 
--- | Downloads an image with the specified URI
--- and converts it to 'P.DynamicImage'.
+-- | Download the image over HTTP
+-- and decode it to 'P.DynamicImage'.
 --
 download :: String -> IO DynamicImage
 download url = do
-  response <- parseRequest url >>= httpBS
 
-  case decodeImage (getResponseBody response) of
-    Right i  -> return i
-    Left err -> error err
+    _ <- if isURI url
+          then return ()
+          else error "Failed to parse the URI"
+
+    response <- (parseRequest url >>= httpBS) 
+                  `E.catch` handler
+
+    case decodeImage (getResponseBody response) of
+      Right i -> return i
+      Left  _ -> error "Failed to decode the image"
+  where 
+    handler = \case
+      HttpExceptionRequest _ (StatusCodeException res _) ->
+        let (Status code msg) = responseStatus res 
+        in error $ concat [ "Failed to download the image ("
+                          , show code
+                          , " "
+                          , show msg
+                          , ")"]
+
+      HttpExceptionRequest _ (ConnectionFailure _) ->
+        error "Failed to download the image: Connection failure"
+      
+      InvalidUrlException _ _ ->
+        error "Failed to download the image: Invalid URL"
+
+      _ -> error "Failed to download the image"
